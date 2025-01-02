dfda7970995d55245430534a2821a6df9927a4c7
LilAguh <aguezeochoa@gmail.com>
2025-01-02 13:10:01 -0300

    Se realiza la configuracion inicial del programs con jwt y cors

--- TournamentAPI/Program.cs
+++ TournamentAPI/Program.cs
@@ -1,15 +1,49 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configuración para CORS
+builder.Services.AddCors(options =>
+{
+    var corsSettings = builder.Configuration.GetSection("CorsSettings");
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins(corsSettings.GetSection("Origins").Get<string[]>())
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+// Configuración para JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,6 +52,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Activar Middleware de CORS, Autenticación y Autorización
+app.UseCors();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
