7d5bc5e8be3abb0e6bdb61238211b8bdba9d80a9
LilAguh <aguezeochoa@gmail.com>
2025-01-07 22:31:57 -0300

    Se agrega la interfaz para el dao de user

---
+++ DataAccess/DAOs/Interfaces/IUserDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Entities;
+
+namespace DataAccess.DAOs.Interfaces
+{
+    public interface IUserDao
+    {
+        Task<User> GetById(int id);
+    }
+}
