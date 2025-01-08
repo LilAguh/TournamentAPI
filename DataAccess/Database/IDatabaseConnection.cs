75a0ddcb0bf5a3b27f0be2878ebeb8b7a04a2cb9
LilAguh <aguezeochoa@gmail.com>
2025-01-07 22:33:21 -0300

    interfaz de mysqlconnectionFactory

---
+++ DataAccess/Database/IDatabaseConnection.cs
@@ -0,0 +1,14 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Database
+{
+    public interface IDatabaseConnection
+    {
+        Task<MySqlConnection> GetConnectionAsync();
+    }
+}
