e3b3fa939a3c57444244691651379e722c6fd2e3
LilAguh <aguezeochoa@gmail.com>
2025-01-07 22:31:27 -0300

    Se crea el dao de usuario

---
+++ DataAccess/DAOs/Implementations/UserDao.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.DAOs.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAOs.Implementations
+{
+    public class UserDao : IUserDao
+    {
+    }
+}
