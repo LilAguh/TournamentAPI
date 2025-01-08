e8f5166d9ce926f30cca9ee49a1504e9ed2caabc
LilAguh <aguezeochoa@gmail.com>
2025-01-07 22:32:45 -0300

    Entidad encargada de el puente entre la base de datos y el dao

---
+++ DataAccess/Database/MySqlConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Database
+{
+    public class MySqlConnectionFactory : IDatabaseConnection
+    {
+        private readonly string _connectionString;
+
+        public MySqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<MySqlConnection> GetConnectionAsync()
+        {
+            var connection = new MySqlConnection(_connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
