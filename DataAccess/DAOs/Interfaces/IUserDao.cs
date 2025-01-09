d23608bb605701f0fa214c006a793eee1082be38
LilAguh <aguezeochoa@gmail.com>
2025-01-09 19:21:47 -0300

    interfaz para adduser, updateuser y deleteduser

--- DataAccess/DAOs/Interfaces/IUserDao.cs
+++ DataAccess/DAOs/Interfaces/IUserDao.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models.DTOs;
 using Models.Entities;
 
 namespace DataAccess.DAOs.Interfaces
@@ -11,5 +12,8 @@ namespace DataAccess.DAOs.Interfaces
     {
         Task<User> GetUserById(int id);
         Task<User> GetUserByEmail(string email);
+        Task AddUser(UserDto userRegisterDto);
+        Task UpdateUser(int id, UserDto userUpdateDto);
+        Task DesactivateUser(int id);
     }
 }
