9d3cb89aaf7ab8644fe3cc5839b33358f150468d
LilAguh <aguezeochoa@gmail.com>
2025-01-09 19:21:23 -0300

    se agrega adduser, updateuser y deleteduser pero logico

--- DataAccess/DAOs/Implementations/UserDao.cs
+++ DataAccess/DAOs/Implementations/UserDao.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using DataAccess.DAOs.Interfaces;
 using DataAccess.Database;
+using Models.DTOs;
 using Models.Entities;
 using System;
 using System.Collections.Generic;
@@ -34,6 +35,54 @@ namespace DataAccess.DAOs.Implementations
             {
                 var query = "SELECT * FROM Users WHERE Email = @Email";
                 return await connection.QueryFirstOrDefaultAsync<User>(query, new { Email = email });
+            }
+        }
+
+        public async Task AddUser(UserDto userRegisterDto)
+        {
+            using (var connection = await _databaseConnection.GetConnectionAsync())
+            {
+                var query = @"
+                INSERT INTO Users (Name, LastName, Alias, Email, Password, Country, Role, AvatarUrl, Active)
+                VALUES (@Name, @LastName, @Alias, @Email, @Password, @Country, @Role, @AvatarUrl, @Active)";
+                await connection.ExecuteAsync(query, userRegisterDto);
+            }
+        }
+
+        public async Task UpdateUser (int id, UserDto userUpdateDto)
+        {
+            using (var connection = await _databaseConnection.GetConnectionAsync())
+            {
+                var query = @"
+                UPDATE Users
+                SET Name = @Name, LastName = @LastName, Alias = @Alias, Email = @Email,
+                    Password = @Password, Country = @Country, Role = @Role,
+                    AvatarUrl = @AvatarUrl
+                WHERE Id = @Id";
+
+                var parameters = new
+                {
+                    Id = id,
+                    userUpdateDto.Name,
+                    userUpdateDto.LastName,
+                    userUpdateDto.Alias,
+                    userUpdateDto.Email,
+                    userUpdateDto.Password,
+                    userUpdateDto.Country,
+                    userUpdateDto.Role,
+                    userUpdateDto.AvatarUrl
+                };
+
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task DesactivateUser (int id)
+        {
+            using (var connection = await _databaseConnection.GetConnectionAsync())
+            {
+                var query = "UPDATE Users SET Active = false WHERE Id = @Id";
+                await connection.ExecuteAsync(query, new { Id = id });
             }
         }
     }
