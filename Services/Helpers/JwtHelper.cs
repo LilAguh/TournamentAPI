f86953f607cbe75acc48eac29ebf9fe87d9e0d17
LilAguh <aguezeochoa@gmail.com>
2025-01-12 19:52:58 -0300

    Se crea jwthelper que se encarga de generar los tokens

---
+++ Services/Helpers/JwtHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Models.DTOs;
+using Models.Entities;
+using Config;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Helpers
+{
+    public class JwtHelper
+    {
+        private readonly JwtConfig _jwtConfig;
+
+        public JwtHelper(JwtConfig jwtConfig)
+        {
+            _jwtConfig = jwtConfig;
+        }
+
+        public string GenerateToken(UserDto userDto)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfig.Key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userDto.ID.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, userDto.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Role, userDto.Role)
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtConfig.Issuer,
+                audience: _jwtConfig.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_jwtConfig.ExpireMinutes),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
