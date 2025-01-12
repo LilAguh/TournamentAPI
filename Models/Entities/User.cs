a223cbec2b2f3b872cb2d4e0638a4fb33f454cac
LilAguh <aguezeochoa@gmail.com>
2025-01-12 19:49:45 -0300

    Se le elimina la condicion a los strings

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -8,15 +8,15 @@ namespace Models.Entities
 {
     public class User
     {
-        public int ID { get; set; }
-        public string? Name { get; set; }
-        public string? LastName { get; set; }
-        public string? Alias { get; set; }
-        public string? Email { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Alias { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
-        public string? Country { get; set; }
-        public string? Role { get; set; }
-        public string? AvatarUrl { get; set; }
+        public string Country { get; set; }
+        public string Role { get; set; }
+        public string AvatarUrl { get; set; }
         public bool Active { get; set; } = true;
     }
 }
