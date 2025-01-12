1dd72a83e1b5e791fee1543c8db2f8af5e425f23
LilAguh <aguezeochoa@gmail.com>
2025-01-12 19:54:45 -0300

    Se inicializan los userDao y los helpers en el proyecto

--- Services/Implementations/UserService.cs
+++ Services/Implementations/UserService.cs
@@ -1,13 +1,25 @@
-﻿using Services.Interfaces;
+﻿using DataAccess.DAOs.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Services.Helpers;
 
 namespace Services.Implementations
 {
     public class UserService : IUserService
     {
+        private readonly IUserDao _userDao;
+        private readonly PasswordHasher _passwordHasher;
+        private readonly JwtHeader _jwtHelper;
+
+        public UserService(IUserDao userDao, PasswordHasher passwordHasher, JwtHelper jwtHelper)
+        {
+
+        }
     }
 }
