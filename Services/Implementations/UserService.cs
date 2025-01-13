46f70e30e1f67a5fb1ee407bfd8648f00fdb6380
LilAguh <aguezeochoa@gmail.com>
2025-01-12 21:46:51 -0300

    Se crea el servicio para el registro de usuario

--- Services/Implementations/UserService.cs
+++ Services/Implementations/UserService.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Services.Helpers;
+using Models.DTOs;
+using System.Linq.Expressions;
 
 namespace Services.Implementations
 {
@@ -22,6 +24,45 @@ namespace Services.Implementations
             _userDao = userDao;
             _passwordHasher = passwordHasher;
             _jwtHelper = jwtHelper;
+        }
+
+        public async Task<UserResponseDto> Register(UserDto userDto)
+        {
+            var existingUser = await _userDao.GetUserByEmail(userDto.Email);
+            if (existingUser != null)
+            {
+                throw new Exception("Email already registered");
+            }
+
+            var passwordHash = _passwordHasher.HashPassword(userDto.Password);
+
+            var newUser = new UserDto
+            {
+                Name = userDto.Name,
+                LastName = userDto.LastName,
+                Alias = userDto.Alias,
+                Email = userDto.Email,
+                Password = passwordHash,
+                Country = userDto.Country,
+                Role = userDto.Role,
+                AvatarUrl = userDto.AvatarUrl,
+                Active = true
+            };
+
+            await _userDao.AddUser(newUser);
+
+            var user = await _userDao.GetUserByEmail(userDto.Email);
+            return new UserResponseDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                LastName = user.LastName,
+                Alias = user.Alias,
+                Email = user.Email,
+                Country = user.Country,
+                Role = user.Role,
+                AvatarUrl = user.AvatarUrl
+            };
         }
     }
 }
