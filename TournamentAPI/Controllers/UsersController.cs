9b7bc0ca9bf53e6611c7c21b38656f53d698b4d2
LilAguh <aguezeochoa@gmail.com>
2025-01-14 20:44:20 -0300

    Se completan los controladores

--- TournamentAPI/Controllers/UsersController.cs
+++ TournamentAPI/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models.DTOs;
 using Services.Interfaces;
 
 namespace TournamentAPI.Controllers
@@ -13,6 +15,44 @@ namespace TournamentAPI.Controllers
         public UsersController (IUserService userService)
         {
             _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            var userResponse = await _userService.Register(userDto);
+            return Ok(userResponse);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserDto userDto)
+        {
+            var token = await _userService.Login(userDto);
+            return Ok(new { Token = token });
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var userResponse = await _userService.GetUserById(id);
+            return Ok(userResponse);
+        }
+
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserDto userDto)
+        {
+            await _userService.UpdateUser(id, userDto);
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpPut("{id}/deactivate")]
+        public async Task<IActionResult> DeactivateUser(int id)
+        {
+            await _userService.DeactivateUser(id);
+            return NoContent();
         }
     }
 }
