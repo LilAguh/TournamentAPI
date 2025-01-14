11c77ce61c1cb19ae25cb1e8e41f8fde60321f7d
LilAguh <aguezeochoa@gmail.com>
2025-01-13 23:06:25 -0300

    Se crea el controlador de usuarios

---
+++ TournamentAPI/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace TournamentAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController (IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
