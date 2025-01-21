0ff4ffe8c79e1da0ce0df0e4da9ce7edec789633
LilAguh <aguezeochoa@gmail.com>
2025-01-21 04:01:45 -0300

    interface para GetUserByAlias

--- DataAccess/DAOs/Interfaces/IUserDao.cs
+++ DataAccess/DAOs/Interfaces/IUserDao.cs
@@ -7,6 +7,7 @@ namespace DataAccess.DAOs.Interfaces
     {
         Task<User> GetUserById(int id);
         Task<User> GetUserByEmail(string email);
+        Task<User> GetUserByAlias(string alias);
         Task AddUser(UserDto userRegisterDto);
         Task UpdateUser(int id, UserDto userUpdateDto);
         Task DeactivateUser(int id);
