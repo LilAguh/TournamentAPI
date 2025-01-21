fd88a9a8a08c7a2870828fd11ac55fb514f49b0a
LilAguh <aguezeochoa@gmail.com>
2025-01-21 03:44:03 -0300

    Interfaz para RegisterPlayer

--- Services/Interfaces/IUserService.cs
+++ Services/Interfaces/IUserService.cs
@@ -5,6 +5,7 @@ namespace Services.Interfaces
     public interface IUserService
     {
         Task<UserResponseDto> Register(UserDto userDto);
+        Task<UserResponseDto> RegisterPlayer(PlayerRegisterDto playerRegisterDto);
         Task<string> Login(UserLoginDto userLoginDto);
         Task<UserResponseDto> GetUserById(int id);
         Task UpdateUser(int id, UserDto userDto);
