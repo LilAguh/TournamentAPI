e006564a3e037989c0892bd0af89ff090714770e
LilAguh <aguezeochoa@gmail.com>
2025-01-21 19:47:18 -0300

    Interfaz de RegisterAdmin

--- Services/Interfaces/IUserService.cs
+++ Services/Interfaces/IUserService.cs
@@ -6,6 +6,7 @@ namespace Services.Interfaces
     {
         Task<UserResponseDto> Register(UserDto userDto);
         Task<UserResponseDto> RegisterPlayer(PlayerRegisterDto playerRegisterDto);
+        Task<UserResponseDto> RegisterAdmin(AdminsRegisterDto adminsRegisterDto, int createdBy);
         Task<string> Login(UserLoginDto userLoginDto);
         Task<UserResponseDto> GetUserById(int id);
         Task UpdateUser(int id, UserDto userDto);
