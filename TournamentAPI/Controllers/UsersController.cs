9239fea1e9531b85d97428c98584f8742f1d8ec0
LilAguh <aguezeochoa@gmail.com>
2025-01-21 19:55:26 -0300

    Se crea el post para registrar a travez del administrador

--- TournamentAPI/Controllers/UsersController.cs
+++ TournamentAPI/Controllers/UsersController.cs
@@ -50,6 +50,26 @@ namespace TournamentAPI.Controllers
             }
         }
 
+        [Authorize(Roles = "admin")]
+        [HttpPost("register/admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] AdminsRegisterDto adminsRegisterDto)
+        {
+            try
+            {
+                var createdById = int.Parse(User.FindFirst("sub")?.Value ?? throw new UnauthorizedAccessException("User ID not found."));
+                var userResponse = await _userService.RegisterAdmin(adminsRegisterDto, createdById);
+                return Ok(userResponse);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An unexpected error occurred.", details = ex.Message });
+            }
+        }
+
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserLoginDto userLoginDto)
