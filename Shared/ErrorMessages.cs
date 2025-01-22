80bdaf90682e3bf6a51a062267fccf48376bbb12
LilAguh <aguezeochoa@gmail.com>
2025-01-22 13:05:41 -0300

    Archivo dedicado a los mensajes de error

---
+++ Shared/ErrorMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared
+{
+    public class ErrorMessages
+    {
+        public const string InvalidInput = "Invalid input data.";
+        public const string EmailAlreadyRegistered = "The email is already registered.";
+        public const string AliasAlreadyRegistered = "The alias is already in use.";
+        public const string PasswordTooShort = "Password must be at least 8 characters long.";
+        public const string InvalidEmailFormat = "Invalid email format.";
+    }
+}
