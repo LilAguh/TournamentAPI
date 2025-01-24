5074936abb789543fbdd3efa55a97b31cb7469bc
LilAguh <aguezeochoa@gmail.com>
2025-01-24 04:03:29 -0300

    Se agrega datos de fecha de creacion de usuario y de ultimo logeo

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -3,16 +3,18 @@ namespace Models.Entities
 {
     public class User
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Role { get; set; } = "Player";  // Default role is "Player"
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Alias { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Country { get; set; }
-        public string Role { get; set; }
-        public string Avatar { get; set; }
-        public bool Active { get; set; } = true;
-        public int CreatedBy { get; set; }
+        public string CountryCode { get; set; }
+        public string AvatarUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;  // Default to current UTC time
+        public DateTime? LastLogin { get; set; }  // Nullable for initial creation
+        public bool IsActive { get; set; } = true;  // Default to active
+        public int CreatedBy { get; set; } = 0;  // Default to 0 (not created by anyone)
     }
 }
