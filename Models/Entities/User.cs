51b969cef73127eb54539386670c7f1a4cdeab83
LilAguh <aguezeochoa@gmail.com>
2025-01-24 16:37:40 -0300

    Modelo de usuarios

---
+++ Models/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Alias { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string CountryCode { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public bool IsActive { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
