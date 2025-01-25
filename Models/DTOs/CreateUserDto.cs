0f8ca14765cd385b70ffc968f767110d02c4264a
LilAguh <aguezeochoa@gmail.com>
2025-01-25 03:26:38 -0300

    Se cambia createplayer por createuser

---
+++ Models/DTOs/CreateUserDto.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Models.DTOs
+{
+    public class CreateUserDto
+    {
+        public string Role { get; set; } = "Player";  // Valor predeterminado
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Alias { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string CountryCode { get; set; }
+        public string AvatarUrl { get; set; }  // Cambiado de ImageUrl a AvatarUrl
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;  // Valor predeterminado
+        public bool IsActive { get; set; } = true;  // Valor predeterminado
+        public int CreatedBy { get; set; } = 0;  // Valor predeterminado
+    }
+}
