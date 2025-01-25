7862fe6c3f54930107c144c0070bcfc54c5aebd6
LilAguh <aguezeochoa@gmail.com>
2025-01-25 03:29:34 -0300

    Servicio de usuario

---
+++ Services/Implementations/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using Models.DTOs;
+using Models.Entities;
+
+namespace Services.Implementations
+{
+    public interface IUserService
+    {
+        Task<User> Register(PlayerRegisterDto dto);
+    }
+}
