946eac74e86f2ca516b0496c252e9966d411ae1d
LilAguh <aguezeochoa@gmail.com>
2025-01-25 16:12:50 -0300

    Exception pasa a estar directamente en el servicio

---
+++ Exceptions/CountryCodeValidation.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using DataAccess.DAOs.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Models.Exceptions
+{
+    public class CountryCodeValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var countryCode = value?.ToString();
+            if (string.IsNullOrEmpty(countryCode))
+            {
+                return new ValidationResult("Codigo de pais requerido");
+            }
+
+            var countryDao = validationContext.GetService<ICountryDao>();
+
+            if (countryDao == null)
+                throw new InvalidOperationException("Servicio de paises no registrado");
+
+            var countryExist = countryDao.CountryExists(countryCode).Result;
+            return countryExist ? ValidationResult.Success : new ValidationResult("El Codigo de pais no existe");
+        }
+    }
+}
