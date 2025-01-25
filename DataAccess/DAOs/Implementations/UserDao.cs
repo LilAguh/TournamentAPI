22a03b4adb4189bdbd3bd537a19338a4eadc08a4
LilAguh <aguezeochoa@gmail.com>
2025-01-25 03:28:01 -0300

    dao de usuario

---
+++ DataAccess/DAOs/Implementations/UserDao.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using MySqlConnector;
+using Models.DTOs;
+using Config;
+using Microsoft.Extensions.Options;
+using DataAccess.DAOs.Interfaces;
+using Models.Entities;
+using DataAccess.Database;
+
+namespace DataAccess.DAOs.Implementations
+{
+    public class UserDao : IUserDao
+    {
+        private readonly IDatabaseConnection _databaseConnection;
+
+        public UserDao(IDatabaseConnection databaseConnection)
+        {
+            _databaseConnection = databaseConnection;
+        }
+
+
+        //public async Task AddUserAsync(CreateUserDto userDto)
+        //{
+        //    using var connection = new MySqlConnection(_databaseConfig.ConnectionString);
+        //    var query = """
+        //        INSERT INTO users (role, first_name, last_name, alias, email, password_hash, country_code, avatar, created_at, active, created_by)
+        //        VALUES (@Role, @FirstName, @LastName, @Alias, @Email, @PasswordHash, @CountryCode, @AvatarUrl, @CreatedAt, @IsActive, @CreatedBy);
+        //    """;
+        //    await connection.ExecuteAsync(query, userDto);
+        //}
+
+        public async Task AddUserAsync(CreateUserDto userDto)
+        {
+            using (var connection = await _databaseConnection.GetConnectionAsync())
+            {
+                var query = @"
+                INSERT INTO users (role, first_name, last_name, alias, email, password_hash, country_code, avatar, created_at, active, created_by)
+                VALUES (@Role, @FirstName, @LastName, @Alias, @Email, @PasswordHash, @CountryCode, @AvatarUrl, @CreatedAt, @IsActive, @CreatedBy)";
+
+                await connection.ExecuteAsync(query, userDto);
+            }
+        }
+
+
+        public async Task<User> GetUserByEmailOrAliasAsync(string emailOrAlias)
+        {
+            using (var connection = await _databaseConnection.GetConnectionAsync())
+            {
+                var query = "SELECT * FROM users WHERE email = @EmailOrAlias OR alias = @EmailOrAlias";
+                return await connection.QueryFirstOrDefaultAsync<User>(query, new { EmailOrAlias = emailOrAlias });
+            }
+        }
+
+    }
+}
