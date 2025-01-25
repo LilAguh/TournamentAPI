a06ca33295877db18e39582df357d1c6b347074c
LilAguh <aguezeochoa@gmail.com>
2025-01-25 03:26:58 -0300

    Dto para el registro de jugador

---
+++ Models/DTOs/PlayerRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Models.DTOs
+{
+    public class PlayerRegisterDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Alias { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string CountryCode { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+    }
