9ff60938ceb512949d35bdfd8781cccec060b9e5
LilAguh <aguezeochoa@gmail.com>
2025-02-01 16:20:47 -0300

    Se crea un dto para la actualizacion de la contraseña

---
+++ Models/DTOs/ChangePasswordDto.cs
@@ -0,0 +1,15 @@
+﻿using Config;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.DTOs
+{
+    public class ChangePasswordDto
+    {
+        [Required(ErrorMessage = ErrorMessages.RequiredPassword)]
+        public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.RequiredPassword)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = ErrorMessages.InvalidPassword)]
+        public string NewPassword { get; set; }
+    }
+}
