8721cd3caf711f3623a63ee29b5a07340b69130a
LilAguh <aguezeochoa@gmail.com>
2025-02-01 16:22:24 -0300

    Se crea la interfaz de changePassword

--- Services/Interfaces/IUserService.cs
+++ Services/Interfaces/IUserService.cs
@@ -9,6 +9,7 @@ namespace Services.Interfaces
         Task<User> Register(PlayerRegisterDto dto);
         Task<User> CreateUserByAdmin(AdminRegisterDto dto, int adminId);
         Task<User> UpdateUser(int id, UpdateUserDto dto);
+        Task ChangePasswordAsync(int userId, ChangePasswordDto dto);
         Task DeleteUser(int id);
         Task<User> GetUserById(int id);
     }
