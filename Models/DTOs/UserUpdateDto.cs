b876e5edbbdbf4e82fa39483c1388126ebf2a959
LilAguh <aguezeochoa@gmail.com>
2025-02-02 17:55:12 -0300

    Dto de actualizacion de usuarios para integracion en el dao

---
+++ Models/DTOs/UserUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Models.DTOs
+{
+    public class UserUpdateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Alias { get; set; }
+        public string Email { get; set; }
+        public string CountryCode { get; set; }
+        public string AvatarUrl { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
