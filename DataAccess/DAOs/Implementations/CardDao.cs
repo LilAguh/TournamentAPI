a3a623817107a0f5ce76d32abe46dbe59285e7cb
LilAguh <aguezeochoa@gmail.com>
2025-02-03 12:59:19 -0300

    Metodo del dao UpdateCardAsync

--- DataAccess/DAOs/Implementations/CardDao.cs
+++ DataAccess/DAOs/Implementations/CardDao.cs
@@ -15,15 +15,22 @@ namespace DataAccess.DAOs.Implementations
             _databaseConnection = databaseConnection;
         }
 
-        public async Task<int> AddCardAsync (CardRequestDto card, int adminId)
+        public async Task<int> AddCardAsync(CardRequestDto card, int adminId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
             var query = @"
-                INSERT INTO Cards (Name, Attack, Defense, IllustrationUrl, CreatedBy) 
-                VALUES (@Name, @Attack, @Defense, @IllustrationUrl, @CreatedBy);
-                SELECT LAST_INSERT_ID();";
+            INSERT INTO Cards (Name, Attack, Defense, IllustrationUrl, CreatedBy) 
+            VALUES (@Name, @Attack, @Defense, @IllustrationUrl, @CreatedBy);
+            SELECT LAST_INSERT_ID();";
 
-            return await connection.ExecuteScalarAsync<int>(query, new NewCardDto());
+            return await connection.ExecuteScalarAsync<int>(query, new
+            {
+                card.Name,
+                card.Attack,
+                card.Defense,
+                card.IllustrationUrl,
+                CreatedBy = adminId
+            });
         }
 
         public async Task<IEnumerable<CardResponseDto>> GetAllCardsAsync()
@@ -38,6 +45,25 @@ namespace DataAccess.DAOs.Implementations
             using var connection = await _databaseConnection.GetConnectionAsync();
             var query = "SELECT * FROM Cards WHERE ID = @Id";
             return await connection.QueryFirstOrDefaultAsync<CardResponseDto>(query, new { Id = id });
+        }
+
+        public async Task<bool> UpdateCardAsync(int id, CardRequestDto card)
+        {
+            using var connection = await _databaseConnection.GetConnectionAsync();
+            var query = @"
+            UPDATE Cards 
+            SET Name = @Name, Attack = @Attack, Defense = @Defense, IllustrationUrl = @IllustrationUrl
+            WHERE ID = @Id";
+
+            var rowsAffected = await connection.ExecuteAsync(query, new
+            {
+                Id = id,
+                card.Name,
+                card.Attack,
+                card.Defense,
+                card.IllustrationUrl
+            });
+            return rowsAffected > 0;
         }
     }
 }
