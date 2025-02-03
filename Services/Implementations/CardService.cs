d8936dc60646ef6ea02dff603c222b71af0973b2
LilAguh <aguezeochoa@gmail.com>
2025-02-03 12:33:08 -0300

    servicio crea carta, recupera todas las cartas y recupera cartas por id

--- Services/Implementations/CardService.cs
+++ Services/Implementations/CardService.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using DataAccess.DAOs.Interfaces;
+using Models.DTOs.Cards;
+using Services.Interfaces;
 
 namespace Services.Implementations
 {
-    internal class CardService
+    public class CardService : ICardService
     {
+        private readonly ICardDao _cardDao;
+
+        public CardService(ICardDao cardDao)
+        {
+            _cardDao = cardDao;
+        }
+
+        public async Task<int> CreateCardAsync (CardRequestDto card, int adminId)
+        {
+            return await _cardDao.AddCardAsync(card, adminId);
+        }
+
+        public async Task<IEnumerable<CardResponseDto>> GetAllCardsAsync()
+        {
+            return await _cardDao.GetAllCardsAsync();
+        }
+
+        public async Task<CardResponseDto?> GetCardByIdAsync(int id)
+        {
+            return await _cardDao.GetCardByIdAsync(id);
+        }
+
     }
 }
