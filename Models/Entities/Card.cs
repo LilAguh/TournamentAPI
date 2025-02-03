4b79191ad0e96c8396453f5eed351046bd8e6679
LilAguh <aguezeochoa@gmail.com>
2025-02-03 07:53:32 -0300

    Se crea modelo para cartas

---
+++ Models/Entities/Card.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Models.Entities
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public string? IllustrationUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
