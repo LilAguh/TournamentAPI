7dc773db81897c0277374c1a872d95f41f1cefbc
LilAguh <aguezeochoa@gmail.com>
2025-02-03 12:15:07 -0300

    controlador para crear cartas y recuperar a travez de id

--- TournamentApiV2/Controllers/CardController.cs
+++ TournamentApiV2/Controllers/CardController.cs
@@ -1,9 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Models.DTOs.Cards;
+using Services.Implementations;
+using System.Security.Claims;
 
 namespace TournamentApiV2.Controllers
 {
-    public class CardController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : ControllerBase
     {
-        
+        private readonly CardService _cardService;
+
+        public CardController(CardService cardService)
+        {
+            _cardService = cardService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCard([FromBody] CardRequestDto card)
+        {
+            var adminId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "0");
+            var cardId = await _cardService.CreateCardAsync(card, 21);
+            return CreatedAtAction(nameof(GetCardById), new { id = cardId }, new { id = cardId });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCardById(int id)
+        {
+            var card = await _cardService.GetCardByIdAsync(id);
+            if (card == null)
+                return NotFound("Carta no encontrada");
+
+            return Ok(card);
+        }
     }
 }
