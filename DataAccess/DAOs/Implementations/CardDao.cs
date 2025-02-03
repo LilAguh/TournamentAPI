38c10c7c4db4fe9dfcb25f5e89be120f742652cd
LilAguh <aguezeochoa@gmail.com>
2025-02-03 12:34:12 -0300

    Dao carga nuevas cartas, trae todas las cartas y por id

--- DataAccess/DAOs/Implementations/CardDao.cs
+++ DataAccess/DAOs/Implementations/CardDao.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Dapper;
+using DataAccess.DAOs.Interfaces;
+using DataAccess.Database;
+using Models.DTOs.Cards;
 
 namespace DataAccess.DAOs.Implementations
 {
-    internal class CardDao
+    public class CardDao : ICardDao
     {
+        private readonly IDatabaseConnection _databaseConnection;
+
+        public CardDao(IDatabaseConnection databaseConnection)
+        {
+            _databaseConnection = databaseConnection;
+        }
+
+        public async Task<int> AddCardAsync (CardRequestDto card, int adminId)
+        {
+            using var connection = await _databaseConnection.GetConnectionAsync();
+            var query = @"
+                INSERT INTO Cards (Name, Attack, Defense, IllustrationUrl, CreatedBy) 
+                VALUES (@Name, @Attack, @Defense, @IllustrationUrl, @CreatedBy);
+                SELECT LAST_INSERT_ID();";
+
+            return await connection.ExecuteScalarAsync<int>(query, new NewCardDto());
+        }
+
+        public async Task<IEnumerable<CardResponseDto>> GetAllCardsAsync()
+        {
+            using var connection = await _databaseConnection.GetConnectionAsync();
+            var query = "SELECT * FROM Cards";
+            return await connection.QueryAsync<CardResponseDto>(query);
+        }
+
+        public async Task<CardResponseDto?> GetCardByIdAsync(int id)
+        {
+            using var connection = await _databaseConnection.GetConnectionAsync();
+            var query = "SELECT * FROM Cards WHERE ID = @Id";
+            return await connection.QueryFirstOrDefaultAsync<CardResponseDto>(query, new { Id = id });
+        }
     }
 }
