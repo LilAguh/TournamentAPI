258b62cab0ac9942e6be758799329a958b4a2556
LilAguh <aguezeochoa@gmail.com>
2025-02-03 08:01:48 -0300

    dto de respuestas para cartas

---
+++ Models/DTOs/Cards/CardResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.Cards
+{
+    public class CardResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public string IllustrationUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
