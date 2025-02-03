e459e216f3642b86ff180c7f5b20678cc203d352
LilAguh <aguezeochoa@gmail.com>
2025-02-03 20:40:54 -0300

    Se crea el modelo de userCard

---
+++ Models/Entities/UserCards.cs
@@ -0,0 +1,12 @@
+﻿
+using Models.Entities;
+
+namespace Models.Entities
+{
+    public class UserCards
+    {
+        public int UserID { get; set; }
+        public int CardID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
