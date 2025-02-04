a5224572c98fe776c3eba934d964a0e87b993a6c
LilAguh <aguezeochoa@gmail.com>
2025-02-03 21:22:11 -0300

    Se crea el controlador de las cartas del usuario

---
+++ TournamentApiV2/Controllers/UserCardsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TournamentApiV2.Controllers
+{
+    public class UserCardsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
