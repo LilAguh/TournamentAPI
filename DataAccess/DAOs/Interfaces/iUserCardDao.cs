4b005ea0abc417a697058bc1f2e32f214517bf4a
LilAguh <aguezeochoa@gmail.com>
2025-02-04 03:22:06 -0300

    Interfaz del metodo AddUserCardAsync

--- DataAccess/DAOs/Interfaces/iUserCardDao.cs
+++ DataAccess/DAOs/Interfaces/iUserCardDao.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Models.DTOs.UserCards;
 
 namespace DataAccess.DAOs.Interfaces
 {
-    internal class iUserCardDao
+    public interface IUserCardDao
     {
+        Task<bool> AddUserCardAsync(int userId, AddUserCardRequestDto dto);
     }
 }
