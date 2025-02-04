2f0c884a72ba12372ec131938904a994ac9feaf4
LilAguh <aguezeochoa@gmail.com>
2025-02-04 03:46:42 -0300

    Se crean 2 metodos nuevos, GetUserCardsAsync y RemoveUserCardAsync. Que sirven para obtener cartas y eliminarlas del stock del jugador

--- DataAccess/DAOs/Implementations/UserCardDao.cs
+++ DataAccess/DAOs/Implementations/UserCardDao.cs
@@ -38,5 +38,47 @@ namespace DataAccess.DAOs.Implementations
                 return rowsAffected > 0;
             }
         }
+
+        public async Task<IEnumerable<UserCardResponseDto>> GetUserCardsAsync(int userId)
+        {
+            using var connection = await _databaseConnection.GetConnectionAsync();
+            // Realizamos un JOIN para obtener información adicional de la carta
+            var query = @"
+            SELECT uc.CardID, c.Name as CardName, uc.Quantity 
+            FROM UserCards uc
+            INNER JOIN Cards c ON uc.CardID = c.ID
+            WHERE uc.UserID = @UserId";
+            return await connection.QueryAsync<UserCardResponseDto>(query, new { UserId = userId });
+        }
+
+        public async Task<bool> RemoveUserCardAsync(int userId, int cardId)
+        {
+            using var connection = await _databaseConnection.GetConnectionAsync();
+
+            // Primero obtenemos la cantidad actual para esa carta
+            var querySelect = "SELECT Quantity FROM UserCards WHERE UserID = @UserId AND CardID = @CardId";
+            var quantity = await connection.QueryFirstOrDefaultAsync<int?>(querySelect, new { UserId = userId, CardId = cardId });
+
+            if (!quantity.HasValue)
+            {
+                // No se encontró la carta en el registro del usuario
+                return false;
+            }
+
+            if (quantity > 1)
+            {
+                // Si tiene más de 1, se decrementa en 1
+                var queryUpdate = "UPDATE UserCards SET Quantity = Quantity - 1 WHERE UserID = @UserId AND CardID = @CardId";
+                var rowsAffected = await connection.ExecuteAsync(queryUpdate, new { UserId = userId, CardId = cardId });
+                return rowsAffected > 0;
+            }
+            else
+            {
+                // Si tiene 1, se elimina la fila
+                var queryDelete = "DELETE FROM UserCards WHERE UserID = @UserId AND CardID = @CardId";
+                var rowsAffected = await connection.ExecuteAsync(queryDelete, new { UserId = userId, CardId = cardId });
+                return rowsAffected > 0;
+            }
+        }
     }
 }
