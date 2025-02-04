3709c3115dba71116cabd93bdc4ac086c6089c0b
LilAguh <aguezeochoa@gmail.com>
2025-02-03 21:10:00 -0300

    dto para registrar cartas del usuario

---
+++ Models/DTOs/UserCards/AddUserCardRequestDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Models.DTOs.UserCards
+{
+    public class AddUserCardRequestDto
+    {
+        public int CardId { get; set; }
+        public int Quantity { get; set; } = 1;
+    }
+}
