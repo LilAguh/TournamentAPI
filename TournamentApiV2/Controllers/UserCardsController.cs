5a6e6f9737a2014c1fa3372b9d9a7929655c6c08
LilAguh <aguezeochoa@gmail.com>
2025-02-04 03:19:30 -0300

    controlador AddUserCard

--- TournamentApiV2/Controllers/UserCardsController.cs
+++ TournamentApiV2/Controllers/UserCardsController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models.DTOs.UserCards;
+using Services.Implementations;
+using System.Security.Claims;
 
 namespace TournamentApiV2.Controllers
 {
-    public class UserCardsController : Controller
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserCardsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly UserCardService _userCardService;
+
+        public UserCardsController(UserCardService userCardService)
         {
-            return View();
+            _userCardService = userCardService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUserCard([FromBody] AddUserCardRequestDto dto)
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "0");
+            var success = await _userCardService.AddUserCardAsync(userId, dto);
+            return success ? Ok("Carta acreditada exitosamente") : BadRequest("Error al acreditar la carta");
         }
     }
 }
