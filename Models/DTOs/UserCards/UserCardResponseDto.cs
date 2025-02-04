600ba6e3174b8fa11319c5614d2728085959ba42
LilAguh <aguezeochoa@gmail.com>
2025-02-03 21:09:37 -0300

    Dto de respuesta de informacion de las cartas del usuario

---
+++ Models/DTOs/UserCards/UserCardResponseDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Models.DTOs.UserCards
+{
+    public class UserCardResponseDto
+    {
+        public int CardId { get; set; }
+        public string CardName { get; set; } = string.Empty;
+        //Por el momento solo nombre, veremos despues como armamos esto
+        public int Quantity { get; set; }
+    }
+}
