168275e0b888945587509b723e80c83b99e49ec0
LilAguh <aguezeochoa@gmail.com>
2025-02-04 03:47:13 -0300

    Se agregan a la interfaz GetUserCardsAsync y RemoveUserCardAsync

--- DataAccess/DAOs/Interfaces/iUserCardDao.cs
+++ DataAccess/DAOs/Interfaces/iUserCardDao.cs
@@ -6,5 +6,7 @@ namespace DataAccess.DAOs.Interfaces
     public interface IUserCardDao
     {
         Task<bool> AddUserCardAsync(int userId, AddUserCardRequestDto dto);
+        Task<IEnumerable<UserCardResponseDto>> GetUserCardsAsync(int userId);
+        Task<bool> RemoveUserCardAsync(int userId, int cardId);
     }
 }
