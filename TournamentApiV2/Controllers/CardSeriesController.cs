4deb11eef7abdf96fc011d1ff791ceca269cc79b
LilAguh <aguezeochoa@gmail.com>
2025-02-05 14:09:56 -0300

    Controlador de las cartas de la serie

---
+++ TournamentApiV2/Controllers/CardSeriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TournamentApiV2.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/cardseries")]
+    [ApiController]
+    public class CardSeriesController : ControllerBase
+    {
+    }
+}
