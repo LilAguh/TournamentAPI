a833b1dc541ba9b2e857efec3626898fadf48183
LilAguh <aguezeochoa@gmail.com>
2025-02-05 13:57:00 -0300

    dto que devuelve la relacion entre la carta y la serie

---
+++ Models/DTOs/CardSeries/CardSeriesResponseDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Models.DTOs.CardSeries
+{
+    public class CardSeriesResponseDto
+    {
+        public int CardId { get; set; }
+        public int SeriesId { get; set; }
+        public string CardName { get; set; }
+        public string SeriesName { get; set; }
+    }
+}
