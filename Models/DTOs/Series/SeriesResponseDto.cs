7e5cc6b05d8c56a17acb73b142d28a2bd0a5153c
LilAguh <aguezeochoa@gmail.com>
2025-02-05 13:51:16 -0300

    Dto para devolver la informacion de las series

---
+++ Models/DTOs/Series/SeriesResponseDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Models.DTOs.Series
+{
+    public class SeriesResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
