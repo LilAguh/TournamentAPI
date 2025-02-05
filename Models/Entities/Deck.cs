335987215ab15e2363c202f4ac2756f7dd44e769
LilAguh <aguezeochoa@gmail.com>
2025-02-04 22:30:27 -0300

    Modelo de los mazos

---
+++ Models/Entities/Deck.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Models.Entities
+{
+    public class Deck
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
