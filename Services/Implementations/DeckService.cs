e6ec933f1eec58e3582868b761f2d43b658bf425
LilAguh <aguezeochoa@gmail.com>
2025-02-05 03:41:04 -0300

    Se añade la logica del servicio para los mazos

---
+++ Services/Implementations/DeckService.cs
@@ -0,0 +1,39 @@
+﻿
+using Models.DTOs.Decks;
+using Services.Interfaces;
+
+namespace Services.Implementations
+{
+    public class DeckService
+    {
+        private readonly IDeckDao _deckDao;
+
+        public DeckService(IDeckDao deckDao)
+        {
+            _deckDao = deckDao;
+        }
+
+        public async Task<DeckResponseDto> CreateDeckAsync(DeckRequestDto dto, int userId)
+        {
+            int deckId = await _deckDao.CreateDeckAsync(dto, userId);
+            var deck = await _deckDao.GetDeckByIdAsync(deckId);
+            if (deck == null)
+                throw new Exception("Error al crear el mazo.");
+            return deck;
+        }
+        public async Task<IEnumerable<DeckResponseDto>> GetDecksByUserAsync(int userId)
+        {
+            return await _deckDao.GetDecksByUserAsync(userId);
+        }
+
+        public async Task<DeckResponseDto?> GetDeckByIdAsync(int deckId)
+        {
+            return await _deckDao.GetDeckByIdAsync(deckId);
+        }
+
+        public async Task<bool> DeleteDeckAsync(int deckId)
+        {
+            return await _deckDao.DeleteDeckAsync(deckId);
+        }
+    }
+}
