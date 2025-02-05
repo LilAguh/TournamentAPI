8345c28a5ebb1a6f90da4da033e86c88216cad0c
LilAguh <aguezeochoa@gmail.com>
2025-02-05 03:39:17 -0300

    se implementan los metodos del crud en los mazos

---
+++ DataAccess/DAOs/Interfaces/IDeckDao.cs
@@ -0,0 +1,13 @@
+﻿
+using Models.DTOs.Decks;
+
+namespace Services.Interfaces
+{
+    public interface IDeckDao
+    {
+        Task<int> CreateDeckAsync(DeckRequestDto dto, int userId);
+        Task<DeckResponseDto?> GetDeckByIdAsync(int deckId);
+        Task<IEnumerable<DeckResponseDto>> GetDecksByUserAsync(int userId);
+        Task<bool> DeleteDeckAsync(int deckId);
+    }
+}
