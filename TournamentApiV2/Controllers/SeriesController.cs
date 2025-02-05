d8962c72d7c11d6ad056318daf4536427718e490
LilAguh <aguezeochoa@gmail.com>
2025-02-05 14:10:08 -0300

    Controlador de la serie

---
+++ TournamentApiV2/Controllers/SeriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TournamentApiV2.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/series")]
+    [ApiController]
+    public class SeriesController : ControllerBase
+    {
+    }
+}
