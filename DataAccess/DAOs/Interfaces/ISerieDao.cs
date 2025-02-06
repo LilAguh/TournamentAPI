13ea39f00178fb640b8c2f1062aa47954d4e3f50
LilAguh <aguezeochoa@gmail.com>
2025-02-06 07:26:47 -0300

    Crud en el dao de la serie

--- DataAccess/DAOs/Interfaces/ISerieDao.cs
+++ DataAccess/DAOs/Interfaces/ISerieDao.cs
@@ -1,7 +1,14 @@
 ﻿
+using Models.DTOs.Series;
+
 namespace DataAccess.DAOs.Interfaces
 {
     public interface ISerieDao
     {
+        Task<SeriesResponseDto> AddSeriesAsync(SeriesRequestDto dto);
+        Task<IEnumerable<SeriesResponseDto>> GetAllSeriesAsync();
+        Task<SeriesResponseDto?> GetSeriesByIdAsync(int id);
+        Task<SeriesResponseDto?> UpdateSeriesAsync(int id, SeriesRequestDto dto);
+        Task<bool> DeleteSeriesAsync(int id);
     }
 }
