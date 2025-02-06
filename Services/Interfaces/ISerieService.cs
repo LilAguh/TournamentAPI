c14565b3b3a19b6e96fc8fcf247af923d2f3750a
LilAguh <aguezeochoa@gmail.com>
2025-02-06 07:23:36 -0300

    se agrega al servicio el crud de la serie

--- Services/Interfaces/ISerieService.cs
+++ Services/Interfaces/ISerieService.cs
@@ -1,7 +1,14 @@
 ﻿
+using Models.DTOs.Series;
+
 namespace Services.Interfaces
 {
     public interface ISerieService
     {
+        Task<SeriesResponseDto> CreateSeriesAsync(SeriesRequestDto dto);
+        Task<IEnumerable<SeriesResponseDto>> GetAllSeriesAsync();
+        Task<SeriesResponseDto?> GetSeriesByIdAsync(int id);
+        Task<SeriesResponseDto?> UpdateSeriesAsync(int id, SeriesRequestDto dto);
+        Task<bool> DeleteSeriesAsync(int id);
     }
 }
