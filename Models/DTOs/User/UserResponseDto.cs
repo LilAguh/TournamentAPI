0aa5cb791470965388d5bf1b00ce0fcb1429caa0
LilAguh <aguezeochoa@gmail.com>
2025-02-09 06:42:33 -0300

    Se crea DTO para obtener user

---
+++ Models/DTOs/User/UserResponseDto.cs
@@ -0,0 +1,22 @@
+﻿
+using Models.Enums;
+
+namespace Models.DTOs.User
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public RoleEnum Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Alias { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string CountryCode { get; set; }
+        public string AvatarUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public bool IsActive { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
