45e98bf0662c990d3edcd51783df06ca629ecdef
LilAguh <aguezeochoa@gmail.com>
2025-02-10 00:24:00 -0300

    Se le cambia el nombre a LoginRequestDto y se mueve a la carpeta user del dto

---
+++ Models/DTOs/User/LoginRequestDto.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Config;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.DTOs.User
+{
+    public class LoginRequestDto
+    {
+        [Required(ErrorMessage = ErrorMessages.UserDataRequired)]
+        public string EmailOrAlias { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.RequiredPassword)]
+        public string Password { get; set; }
+    }
+}
