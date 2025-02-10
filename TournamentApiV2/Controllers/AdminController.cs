f11f046242c6d3d2fd5d7468feb2dd28a0c15ad7
LilAguh <aguezeochoa@gmail.com>
2025-02-10 15:42:12 -0300

    se elimina codigo no utilizado

--- TournamentApiV2/Controllers/AdminController.cs
+++ TournamentApiV2/Controllers/AdminController.cs
@@ -1,26 +1,22 @@
 ﻿using Config;
-using DataAccess.DAOs.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Models.DTOs.User;
 using Services.Interfaces;
 using System.Security.Claims;
 
-
 namespace TournamentApiV2.Controllers
 {
-    [Authorize(Roles = "Admin")] // Solo accesible por Admins
+    [Authorize(Roles = "Admin")]
     [ApiController]
     [Route("[controller]")]
     public class AdminController : ControllerBase
     {
         private readonly IUserService _userService;
-        private readonly IUserDao _userDao;
 
-        public AdminController(IUserService userService, IUserDao userDao)
+        public AdminController(IUserService userService)
         {
             _userService = userService;
-            _userDao = userDao;
         }
 
         [HttpPost]
@@ -28,13 +24,7 @@ namespace TournamentApiV2.Controllers
         {
             var adminId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
             var user = await _userService.CreateUserByAdmin(dto, adminId);
-
-            return Ok(new
-            {
-                user.Alias,
-                user.Email,
-                user.Role
-            });
+            return Ok(new { user.Alias, user.Email, user.Role });
         }
   
     [HttpGet("{id}")]
