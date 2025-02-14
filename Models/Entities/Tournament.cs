959fc890b595be5fcece8d6e223a43d1f467b7b9
LilAguh <aguezeochoa@gmail.com>
2025-02-14 03:18:06 -0300

    Modelo del torneo

---
+++ Models/Entities/Tournament.cs
@@ -0,0 +1,19 @@
+﻿
+using Models.Enums;
+
+namespace Models.Entities
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OrganizerID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string Country { get; set; }
+        public PhaseEnum Phase { get; set; }
+        public int WinnerID { get; set; }
+    }
+}
