d92c5a6f2aa3e01f278a74bda88ba248f12ab7ac
LilAguh <aguezeochoa@gmail.com>
2025-02-14 03:18:21 -0300

    Enum de las faces del torneo

---
+++ Models/Enums/PhaseEnum.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Models.Enums
+{
+    public enum PhaseEnum
+    {
+        Registration = 1,
+        Closed = 2,
+        Tournament = 3,
+        Finalized = 4,
+        Canceled = 5
+    }
+}
