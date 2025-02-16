6ae4c37b338c643eaeebfb873a71cc4234e9ad60
LilAguh <aguezeochoa@gmail.com>
2025-02-16 05:22:10 -0300

    dto de peticion

---
+++ Models/DTOs/Tournament/TournamentRequestDto.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Models.DTOs.Tournament
+{
+    public class TournamentRequestDto
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string Country { get; set; }
+    }
+}
