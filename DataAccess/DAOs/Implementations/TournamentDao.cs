bca288db7e50c9889d11b23b67e83c38d9f42397
LilAguh <aguezeochoa@gmail.com>
2025-02-17 05:56:15 -0300

    Se crea el dao y el servicio del dao de los torneos

---
+++ DataAccess/DAOs/Implementations/TournamentDao.cs
@@ -0,0 +1,7 @@
+﻿
+namespace DataAccess.DAOs.Implementations
+{
+    public class TournamentDao
+    {
+    }
+}
---
+++ DataAccess/DAOs/Interfaces/ITournamentDao.cs
@@ -0,0 +1,7 @@
+﻿
+namespace DataAccess.DAOs.Interfaces
+{
+    public interface ITournamentDao
+    {
+    }
+}
