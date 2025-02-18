fed5a77031e690f8d2bee992657ed0f1a9dd2900
LilAguh <aguezeochoa@gmail.com>
2025-02-17 21:13:09 -0300

    Modelo de el jugador en el torneo

---
+++ Models/Entities/TournamentPlayer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Models.Entities
+{
+    public class TournamentPlayer
+    {
+        public int TournamentID { get; set; }
+        public int UserID { get; set; }
+        public int DeckId { get; set; }
+    }
+}
