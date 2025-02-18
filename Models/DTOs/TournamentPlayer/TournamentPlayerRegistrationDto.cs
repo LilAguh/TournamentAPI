68ed4cae8496f481b2172025958dc1e5e6364b5b
LilAguh <aguezeochoa@gmail.com>
2025-02-17 21:12:49 -0300

    Dto de registro de jugador en el torneo

---
+++ Models/DTOs/TournamentPlayer/TournamentPlayerRegistrationDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Models.DTOs.TournamentPlayers
+{
+    public class TournamentPlayerRegistrationDto
+    {
+        public int TournamentID { get; set; }
+        public int DeckId { get; set; }
+    }
+}
