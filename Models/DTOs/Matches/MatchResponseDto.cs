ef62f6e5c880c61e82a1333d94277394ae6a3106
LilAguh <aguezeochoa@gmail.com>
2025-02-19 20:17:55 -0300

    dto de respuesta de los juegos

---
+++ Models/DTOs/Matches/MatchResponseDto.cs
@@ -0,0 +1,18 @@
+﻿
+using Models.Enums;
+
+namespace Models.DTOs.Matches
+{
+    public class MatchResponseDto
+    {
+        public int Id { get; set; }
+        public int TournamentID { get; set; }
+        public int MatchNumber { get; set; }
+        public int TotalMatches { get; set; }
+        public int Player1ID { get; set; }
+        public int Player2ID { get; set; }
+        public DateTime MatchStartTime { get; set; }
+        public MatchEnum Status { get; set; }
+        public int WinnerID { get; set; }
+    }
+}
