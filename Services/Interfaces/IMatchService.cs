7a812c1b10bd975aa0136c57110c999355e0f23d
LilAguh <aguezeochoa@gmail.com>
2025-02-25 20:16:04 -0300

    Se cambia el createroundmatch y se integra automaticamente la lista de playersid

--- Services/Interfaces/IMatchService.cs
+++ Services/Interfaces/IMatchService.cs
@@ -5,7 +5,7 @@ namespace Services.Interfaces
 {
     public interface IMatchService
     {
-        Task CreateRoundMatchAsync(int tournamentId, List<int> playerIds);
+        Task CreateRoundMatchAsync(int tournamentId);
         Task<IEnumerable<MatchResponseDto>> GetMatchesByTournamentAsync(int tournamentId);
     }
 }
