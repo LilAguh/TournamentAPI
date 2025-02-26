f9b40a2bc8b6c4715ef38c97358753e9caa3d99f
LilAguh <aguezeochoa@gmail.com>
2025-02-25 23:04:08 -0300

    Se randomizan los match de la primer ronda de playoff, y se hace una verificacion para que no se repitan los jugadores

--- DataAccess/DAOs/Implementations/MatchDao.cs
+++ DataAccess/DAOs/Implementations/MatchDao.cs
@@ -19,8 +19,8 @@ namespace DataAccess.DAOs.Implementations
 
         public async Task CreateRoundMatchAsync(int tournamentId, List<int> playerIds)
         {
-            if (playerIds == null || playerIds.Count % 2 != 0)
-                throw new Exception("La cantidad de jugadores debe ser par para formar los partidos");
+            if (playerIds == null)
+                throw new Exception("La lista de jugadores no puede ser nula");
 
             // Aca algo que se puede hacer es en caso de que solo hay un solo jugador dar el partido como ganado
             // Asi podes sumar mayor cantidad de partidos en un torneo
@@ -28,6 +28,17 @@ namespace DataAccess.DAOs.Implementations
             // Es decir que si un match de 16avos de final no consigue un jugador
             // Este pasa directamente a octavos
 
+
+            // Eliminar duplicados para evitar que un jugador se repita
+            playerIds = playerIds.Distinct().ToList();
+
+
+            // Validar que la cantidad de jugadores es par
+            if (playerIds.Count % 2 != 0)
+                throw new Exception("La cantidad de jugadores debe ser par para formar los partidos");
+
+            // Mezclar aleatoriamente los IDs de los jugadores (para la primera ronda)
+            var randomizedPlayerIds = playerIds.OrderBy(x => Guid.NewGuid()).ToList();
 
             // Se asume que se tiene la información del torneo para asignar el horario de inicio
             using var connection = await _databaseConnection.GetConnectionAsync();
@@ -53,7 +64,7 @@ namespace DataAccess.DAOs.Implementations
             DateTime currentMatchTime = tournamentStartDate.Date + tournamentStartTime;
 
             // Se calcula el total de partidos del bracket: TotalMatches = MaxPlayers - 1
-            int totalMatches = (int)tournament.MaxPlayers - 1;
+            int totalMatches = (int)(tournament.MaxPlayers)  - 1;
 
             // Primera ronda: cada partido reúne 2 jugadores, cantidad de partidos es playerIds.Count /
             int matchNumber = 1;
@@ -61,10 +72,10 @@ namespace DataAccess.DAOs.Implementations
                                 VALUES (@TournamentID, @MatchNumber, @TotalMatches, @Player1ID, @Player2ID, @MatchStartTime, @Status)";
 
             // Recorrer la lista de jugadores emparejándolos
-            for (int i = 0; i < playerIds.Count; i += 2)
+            for (int i = 0; i < randomizedPlayerIds.Count; i +=2)
             {
-                int player1Id = playerIds[i];
-                int player2Id = playerIds[i + 1];
+                int player1Id = randomizedPlayerIds[i];
+                int player2Id = randomizedPlayerIds[i + 1];
 
                 await connection.ExecuteAsync(insertQuery, new
                 {
@@ -84,7 +95,7 @@ namespace DataAccess.DAOs.Implementations
                 // Si se supera el horario diario, avanzar al siguiente día y reiniciar al StartTime
                 if (currentMatchTime.TimeOfDay >= tournamentEndTime)
                 {
-                    currentMatchTime = currentMatchTime.Date.AddDays(1) + ((TimeSpan)tournament.StartTime);
+                    currentMatchTime = currentMatchTime.Date.AddDays(1) + tournamentStartTime;
                 }
             }
         }
