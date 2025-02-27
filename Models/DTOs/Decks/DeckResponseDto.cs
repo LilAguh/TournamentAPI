0346dd28ef570a336d381be52fc3569cfa2f90b4
LilAguh <aguezeochoa@gmail.com>
2025-02-27 03:35:34 -0300

    Se agrega el campo cardCount

--- Models/DTOs/Decks/DeckResponseDto.cs
+++ Models/DTOs/Decks/DeckResponseDto.cs
@@ -5,8 +5,9 @@ namespace Models.DTOs.Decks
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
+        public int CardCount { get; set; }
         // podría incluir una lista de cartas (llamada por el módulo CardDecks)
     }
 }
