07b442587e3f7b07fbdf473a9d4ece20f3ff26a6
LilAguh <aguezeochoa@gmail.com>
2025-02-27 06:31:31 -0300

    Modelo de descalificaciones

---
+++ Models/Entities/Disqualification.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Models.Entities
+{
+    public class Disqualification
+    {
+        public int Id { get; set; }
+        public int TournamentID { get; set; }
+        public int PlayerID { get; set; }
+        public int JudgeID { get; set; }
+        public string Reason { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
