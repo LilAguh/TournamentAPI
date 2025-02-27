c07142fd535bdded1636ea1406a662497020ce53
LilAguh <aguezeochoa@gmail.com>
2025-02-27 06:33:42 -0300

    dto de peticion de las descalificaciones

---
+++ Models/DTOs/Disqualifications/DisqualificationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Models.DTOs.Disqualifications
+{
+    public class DisqualificationRequestDto
+    {
+        public int TournamentID { get; set; }
+        public int PlayerID { get; set; }
+        public string Reason { get; set; }
+    }
+}
