588533cbe4079df217869a3f49775213344d1a1a
LilAguh <aguezeochoa@gmail.com>
2025-02-27 03:35:57 -0300

    se agregan los required en los campos

--- Models/DTOs/TournamentPlayer/TournamentPlayerRegistrationDto.cs
+++ Models/DTOs/TournamentPlayer/TournamentPlayerRegistrationDto.cs
@@ -1,9 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Models.DTOs.TournamentPlayers
 {
     public class TournamentPlayerRegistrationDto
     {
+        [Required]
         public int TournamentID { get; set; }
+
+        [Required]
         public int DeckId { get; set; }
     }
 }
