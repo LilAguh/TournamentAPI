69498541f1d1e9b7ad4fb70c236c85fb7e089e02
LilAguh <aguezeochoa@gmail.com>
2025-02-27 09:38:10 -0300

    peticion de los jueces del torneo

---
+++ Models/DTOs/TournamentJudges/TournamentJudgeRequestDto.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.DTOs.TournamentJudges
+{
+    public class TournamentJudgeRequestDto
+    {
+        [Required(ErrorMessage = "El ID del torneo es obligatorio.")]
+        public int TournamentID { get; set; }
+
+        [Required(ErrorMessage = "El ID del juez es obligatorio.")]
+        public int JudgeID { get; set; }
+    }
+}
