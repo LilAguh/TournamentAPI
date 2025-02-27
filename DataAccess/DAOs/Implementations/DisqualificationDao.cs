1f9c28d01d7356c42ab8b6e562b39ebaf62d79cf
LilAguh <aguezeochoa@gmail.com>
2025-02-27 08:20:08 -0300

    DAo de las descalificaciones

---
+++ DataAccess/DAOs/Implementations/DisqualificationDao.cs
@@ -0,0 +1,36 @@
+﻿
+using Dapper;
+using DataAccess.Database;
+using Models.DTOs.Disqualifications;
+using Models.Entities;
+
+namespace DataAccess.DAOs.Implementations
+{
+    public class DisqualificationDao : IDisqualificationDao
+    {
+        private readonly IDatabaseConnection _databaseConnection;
+
+        public DisqualificationDao(IDatabaseConnection databaseConnection)
+        {
+            _databaseConnection = databaseConnection;
+        }
+
+        public async Task<int> AddDisqualificationAsync(Disqualification disqualification)
+        {
+            using var connection = await _databaseConnection.GetConnectionAsync();
+            var query = @" INSERT INTO Disqualifications (TournamentID, PlayerID, JudgeID, Reason, CreatedAt)
+                           VALUES (@TournamentID, @PlayerID, @JudgeID, @Reason, @CreatedAt);
+                           SELECT LAST_INSERT_ID();";
+            return await connection.ExecuteScalarAsync<int>(query, disqualification);
+        }
+
+        public async Task<IEnumerable<DisqualificationResponseDto>> GetDisqualificationsByTournamentAsync(int tournamentId)
+        {
+            using var connection = await _databaseConnection.GetConnectionAsync();
+            var query = @" SELECT Id, TournamentID, PlayerID, JudgeID, Reason, CreatedAt 
+                           FROM Disqualifications 
+                           WHERE TournamentID = @TournamentID";
+            return await connection.QueryAsync<DisqualificationResponseDto>(query, new { TournamentID = tournamentId });
+        }
+    }
+}
---
+++ DataAccess/DAOs/Interfaces/IDisqualificationDao.cs
@@ -0,0 +1,12 @@
+﻿
+using Models.DTOs.Disqualifications;
+using Models.Entities;
+
+namespace DataAccess.DAOs.Interfaces
+{
+    public interface IDisqualificationDao
+    {
+        Task<int> AddDisqualificationAsync(Disqualification disqualification);
+        Task<IEnumerable<DisqualificationResponseDto>> GetDisqualificationsByTournamentAsync(int tournamentId);
+    }
+}
