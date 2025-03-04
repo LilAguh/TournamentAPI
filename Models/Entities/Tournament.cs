544145151dd4ab51eede26646e0104a222f0ea32
LilAguh <aguezeochoa@gmail.com>
2025-03-03 21:16:02 -0300

    Se eliminan los horarios, solamente quedan las fechas

--- Models/Entities/Tournament.cs
+++ Models/Entities/Tournament.cs
@@ -10,8 +10,6 @@ namespace Models.Entities
         public int OrganizerID { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public TimeSpan StartTime { get; set; }
-        public TimeSpan EndTime { get; set; }
         public string CountryCode { get; set; }
         public int MaxPlayers { get; set; }
         public int MaxGames { get; set; }
