22191edab29f7981ca5913b90a7bfb183ce545c2
LilAguh <aguezeochoa@gmail.com>
2025-03-06 11:50:14 -0300

    Se agregan validaciones

--- Models/DTOs/UserCards/AddUserCardRequestDto.cs
+++ Models/DTOs/UserCards/AddUserCardRequestDto.cs
@@ -1,9 +1,15 @@
 ﻿
+using Config;
+using System.ComponentModel.DataAnnotations;
+
 namespace Models.DTOs.UserCards
 {
     public class AddUserCardRequestDto
     {
+        [Required(ErrorMessage = "El ID de la carta es obligatorio.")]
         public int CardId { get; set; }
+
+        [Required(ErrorMessage = "La cantidad es obligatoria.")]
         public int Quantity { get; set; } = 1;
     }
 }
