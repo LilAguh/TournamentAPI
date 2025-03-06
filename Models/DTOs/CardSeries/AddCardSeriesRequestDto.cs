cbe8d65e4f34009b3ed9fd381c06659ecd2ffc16
LilAguh <aguezeochoa@gmail.com>
2025-03-06 11:49:10 -0300

    Se agregan validaciones

--- Models/DTOs/CardSeries/AddCardSeriesRequestDto.cs
+++ Models/DTOs/CardSeries/AddCardSeriesRequestDto.cs
@@ -1,9 +1,15 @@
 ﻿
+using Config;
+using System.ComponentModel.DataAnnotations;
+
 namespace Models.DTOs.CardSeries
 {
     public class AddCardSeriesRequestDto
     {
+        [Required(ErrorMessage = "El ID de la carta es obligatorio.")]
         public int CardId { get; set; }
+
+        [Required(ErrorMessage = "El ID de la serie es obligatorio.")]
         public int SeriesId { get; set; }
     }
 }
