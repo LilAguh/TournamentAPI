55159283d7a53aaf7cf1b048feb4eecd89ee4782
LilAguh <aguezeochoa@gmail.com>
2025-03-06 13:25:14 -0300

    se cambia el rango de la validacion

--- Models/DTOs/CardDecks/AddCardDeckRequestDto.cs
+++ Models/DTOs/CardDecks/AddCardDeckRequestDto.cs
@@ -6,7 +6,8 @@ namespace Models.DTOs.CardDecks
     public class AddCardDeckRequestDto
     {
         [Required(ErrorMessage = "Debe enviarse al menos una carta.")]
-        [Range(1, 15, ErrorMessage = "Solamente se pueden enviar entre 1 y 15 cartas a cada mazo.")]
+        [MinLength(1, ErrorMessage = "Debe enviarse al menos una carta.")]
+        [MaxLength(15, ErrorMessage = "Solamente se pueden enviar entre 1 y 15 cartas a cada mazo.")]
         public List<int> CardId { get; set; }
     }
 
