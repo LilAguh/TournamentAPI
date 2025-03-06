b75c8dc0fbb609c9c2a898dc05d206ab0786422e
LilAguh <aguezeochoa@gmail.com>
2025-03-06 11:50:23 -0300

    Se agregan validaciones

--- Models/DTOs/User/AdminRegisterRequestDto.cs
+++ Models/DTOs/User/AdminRegisterRequestDto.cs
@@ -12,6 +12,7 @@ namespace Models.DTOs.User
     public class AdminRegisterRequestDto
     {
         [Required(ErrorMessage = ErrorMessages.RequiredName)]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "El nombre no puede tener menos de 5 caracteres, ni más de 50 caracteres.")]
         [StringLength(50, ErrorMessage = ErrorMessages.NameExceedCharacters)]
         public string FirstName { get; set; }
 
