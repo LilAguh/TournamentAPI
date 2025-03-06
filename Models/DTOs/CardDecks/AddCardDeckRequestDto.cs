65e4472930c1124d71f68dab712d846971f50033
LilAguh <aguezeochoa@gmail.com>
2025-03-06 11:48:31 -0300

    Se agregan validaciones

--- Models/DTOs/CardDecks/AddCardDeckRequestDto.cs
+++ Models/DTOs/CardDecks/AddCardDeckRequestDto.cs
@@ -1,9 +1,13 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Models.DTOs.CardDecks
 {
     public class AddCardDeckRequestDto
     {
-        public int CardId { get; set; }
+        [Required(ErrorMessage = "Debe enviarse al menos una carta.")]
+        [Range(1, 15, ErrorMessage = "Solamente se pueden enviar entre 1 y 15 cartas a cada mazo.")]
+        public List<int> CardId { get; set; }
     }
 
 }
