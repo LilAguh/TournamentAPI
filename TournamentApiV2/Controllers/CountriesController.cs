6fe432b03708199f3ba40f6f4356a1af82229834
LilAguh <aguezeochoa@gmail.com>
2025-03-25 16:31:32 -0300

    Country pasa a ser countries para mayor optimizacion de los nombres

---
+++ TournamentApiV2/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace TournamentApiV2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly ICountryService _countryService;
+
+        public CountriesController(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        // GET /Countries
+        // Obtiene y retorna la lista de todos los países.
+        [HttpGet]
+        public async Task<IActionResult> GetAllCountries()
+        {
+            var countries = await _countryService.GetAllCountriesAsync();
+            return Ok(countries);
+        }
+    }
+}
