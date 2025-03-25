987e67b0c47ab0b070d062df9a305487920bf55e
LilAguh <aguezeochoa@gmail.com>
2025-03-24 21:39:43 -0300

    Se crea un controlador para obtener toda la lista de paises

---
+++ TournamentApiV2/Controllers/CountryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace TournamentApiV2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountryController : ControllerBase
+    {
+        private readonly ICountryService _countryService;
+
+        public CountryController(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCountries()
+        {
+            var countries = await _countryService.GetAllCountriesAsync();
+            return Ok(countries);
+        }
+    }
+}
