731d29c515b68e0df68be05bedf28019de2e2a0a
LilAguh <aguezeochoa@gmail.com>
2025-03-26 19:39:02 -0300

    Se mejora el idle y se añade comentarios especificando los metodos

--- DataAccess/DAOs/Implementations/CardDao.cs
+++ DataAccess/DAOs/Implementations/CardDao.cs
@@ -1,5 +1,4 @@
-﻿
-using Dapper;
+﻿using Dapper;
 using DataAccess.DAOs.Interfaces;
 using DataAccess.Database;
 using Models.DTOs.Cards;
@@ -8,20 +7,23 @@ namespace DataAccess.DAOs.Implementations
 {
     public class CardDao : ICardDao
     {
+        // Conexión a la base de datos inyectada
         private readonly IDatabaseConnection _databaseConnection;
 
+        // Constructor que inyecta la conexión a la base de datos
         public CardDao(IDatabaseConnection databaseConnection)
         {
             _databaseConnection = databaseConnection;
         }
 
+        // Agrega una nueva carta a la base de datos y retorna el ID generado.
+        // Se requiere el DTO con los datos de la carta y el ID del admin que la crea.
         public async Task<int> AddCardAsync(CardRequestDto card, int adminId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
-            var query = @"
-            INSERT INTO Cards (Name, Attack, Defense, IllustrationUrl, CreatedBy) 
-            VALUES (@Name, @Attack, @Defense, @IllustrationUrl, @CreatedBy);
-            SELECT LAST_INSERT_ID();";
+            var query = @" INSERT INTO Cards (Name, Attack, Defense, IllustrationUrl, CreatedBy) 
+                           VALUES (@Name, @Attack, @Defense, @IllustrationUrl, @CreatedBy);
+                           SELECT LAST_INSERT_ID();";
 
             return await connection.ExecuteScalarAsync<int>(query, new
             {
@@ -33,6 +35,7 @@ namespace DataAccess.DAOs.Implementations
             });
         }
 
+        // Retorna todas las cartas registradas en la base de datos.
         public async Task<IEnumerable<CardResponseDto>> GetAllCardsAsync()
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
@@ -40,6 +43,7 @@ namespace DataAccess.DAOs.Implementations
             return await connection.QueryAsync<CardResponseDto>(query);
         }
 
+        // Retorna la carta cuyo ID coincide con el proporcionado.
         public async Task<CardResponseDto> GetCardByIdAsync(int id)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
@@ -47,13 +51,14 @@ namespace DataAccess.DAOs.Implementations
             return await connection.QueryFirstOrDefaultAsync<CardResponseDto>(query, new { Id = id });
         }
 
+        // Actualiza los datos de una carta en la base de datos.
+        // Retorna true si la actualización se realizó correctamente.
         public async Task<bool> UpdateCardAsync(int id, CardRequestDto card)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
-            var query = @"
-            UPDATE Cards 
-            SET Name = @Name, Attack = @Attack, Defense = @Defense, IllustrationUrl = @IllustrationUrl
-            WHERE ID = @Id";
+            var query = @" UPDATE Cards 
+                           SET Name = @Name, Attack = @Attack, Defense = @Defense, IllustrationUrl = @IllustrationUrl
+                           WHERE ID = @Id";
 
             var rowsAffected = await connection.ExecuteAsync(query, new
             {
@@ -66,6 +71,8 @@ namespace DataAccess.DAOs.Implementations
             return rowsAffected > 0;
         }
 
+        // Elimina una carta de la base de datos según el ID.
+        // Retorna true si la eliminación se realizó correctamente.
         public async Task<bool> DeleteCardAsync(int id)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
