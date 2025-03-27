51d0d8f7ea3a14cd675a1cd743b48a285a980d2e
LilAguh <aguezeochoa@gmail.com>
2025-03-27 11:44:34 -0300

    Se añade comentarios al codigo y mensajes de error

--- TournamentApiV2/Controllers/CardDecksController.cs
+++ TournamentApiV2/Controllers/CardDecksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿
+using Config;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Models.DTOs.CardDecks;
 using Services.Interfaces;
@@ -17,13 +19,17 @@ namespace TournamentApiV2.Controllers
             _cardDeckService = cardDeckService;
         }
 
+        // POST /CardDecks/{deckId}/Cards
+        // Agrega cartas al mazo especificado usando los datos del DTO.
         [HttpPost]
         public async Task<IActionResult> AddCardToDeck(int deckId, [FromBody] AddCardDeckRequestDto requestDto)
         {
             await _cardDeckService.AddCardToDeckAsync(deckId, requestDto);
-            return Ok(new { message = "Cartas agregadas al mazo" });
+            return Ok(new { message = ErrorMessages.CardsAddDeck });
         }
 
+        // GET /CardDecks/{deckId}/Cards
+        // Retorna la lista de cartas asociadas al mazo indicado.
         [HttpGet]
         public async Task<IActionResult> GetCardsInDeck(int deckId)
         {
@@ -31,11 +37,13 @@ namespace TournamentApiV2.Controllers
             return Ok(cards);
         }
 
+        // DELETE /CardDecks/{deckId}/Cards/{cardId}
+        // Elimina una carta del mazo especificado.
         [HttpDelete("{cardId}")]
         public async Task<IActionResult> RemoveCardFromDeck(int deckId, int cardId)
         {
             await _cardDeckService.RemoveCardFromDeckAsync(deckId, cardId);
-            return Ok("Carta removida del mazo");
+            return Ok(ErrorMessages.CardRemovedDeck);
         }
     }
 }
