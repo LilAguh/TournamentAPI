339b8efaf47479cf7b26bd156f928331260ba482
LilAguh <aguezeochoa@gmail.com>
2025-03-27 01:53:00 -0300

    Se añade range required a la cantidad de cartas

--- Models/DTOs/UserCards/AddUserCardRequestDto.cs
+++ Models/DTOs/UserCards/AddUserCardRequestDto.cs
@@ -6,10 +6,11 @@ namespace Models.DTOs.UserCards
 {
     public class AddUserCardRequestDto
     {
-        [Required(ErrorMessage = "El ID de la carta es obligatorio.")]
+        [Required(ErrorMessage = ErrorMessages.CardIdRequired)]
         public int CardId { get; set; }
 
-        [Required(ErrorMessage = "La cantidad es obligatoria.")]
+        [Required(ErrorMessage = ErrorMessages.AmountRequired)]
+        [Range(1, 100, ErrorMessage = ErrorMessages.ErrorQuantity)]
         public int Quantity { get; set; } = 1;
     }
 }
