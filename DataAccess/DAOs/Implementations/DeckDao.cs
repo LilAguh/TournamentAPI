63549b8270c7e1329d8b709014057dfe7286c256
LilAguh <aguezeochoa@gmail.com>
2025-03-27 11:11:11 -0300

    se agrega Idle y comentarios

--- DataAccess/DAOs/Implementations/DeckDao.cs
+++ DataAccess/DAOs/Implementations/DeckDao.cs
@@ -15,15 +15,17 @@ namespace Services.Implementations
             _databaseConnection = databaseConnection;
         }
 
+        // Crea un nuevo mazo para un usuario y retorna el ID generado.
         public async Task<int> CreateDeckAsync(DeckRequestDto dto, int userId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
-            var query = @"INSERT INTO Decks (UserId, Name) 
-                          VALUES (@UserId, @Name);
-                          SELECT LAST_INSERT_ID();";
+            var query = @" INSERT INTO Decks (UserId, Name) 
+                           VALUES (@UserId, @Name);
+                           SELECT LAST_INSERT_ID();";
             return await connection.ExecuteScalarAsync<int>(query, new { UserId = userId, dto.Name });
         }
 
+        // Obtiene un mazo específico junto con la cantidad de cartas que contiene.
         public async Task<DeckResponseDto?> GetDeckByIdAsync(int deckId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
@@ -36,6 +38,7 @@ namespace Services.Implementations
             return await connection.QueryFirstOrDefaultAsync<DeckResponseDto>(query, new { DeckId = deckId });
         }
 
+        // Obtiene todos los mazos asociados a un usuario.
         public async Task<IEnumerable<DeckResponseDto>> GetDecksByUserAsync(int userId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
@@ -43,6 +46,7 @@ namespace Services.Implementations
             return await connection.QueryAsync<DeckResponseDto>(query, new { UserId = userId });
         }
 
+        // Elimina un mazo según su ID.
         public async Task<bool> DeleteDeckAsync(int deckId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
@@ -51,12 +55,13 @@ namespace Services.Implementations
             return rowsAffected > 0;
         }
 
+        // Verifica si un mazo pertenece a un usuario específico.
         public async Task<bool> IsDeckOwnedByUser(int deckId, int userId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
             var query = "SELECT COUNT(*) FROM Decks WHERE ID = @DeckId AND UserID = @UserId";
             int count = await connection.ExecuteScalarAsync<int>(query, new { DeckId = deckId, UserId = userId });
-            return count > 0; // Devuelve true si el mazo pertenece al usuario, false en caso contrario
+            return count > 0;
         }
     }
 }
