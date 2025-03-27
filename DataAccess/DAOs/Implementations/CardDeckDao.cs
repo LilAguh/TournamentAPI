ccf0b5a35f5510979d2192106362bd46e1a69abb
LilAguh <aguezeochoa@gmail.com>
2025-03-27 11:44:14 -0300

    Se añade comentarios al codigo

--- DataAccess/DAOs/Implementations/CardDeckDao.cs
+++ DataAccess/DAOs/Implementations/CardDeckDao.cs
@@ -15,6 +15,7 @@ namespace DataAccess.DAOs.Implementations
             _databaseConnection = databaseConnection;
         }
 
+        // Agrega una carta a un mazo y retorna true si se realizó la inserción.
         public async Task<bool> AddCardToDeckAsync(int deckId, int cardId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
@@ -23,6 +24,7 @@ namespace DataAccess.DAOs.Implementations
             return rowsAffected > 0;
         }
 
+        // Elimina una carta de un mazo y retorna true si se realizó la eliminación.
         public async Task<bool> RemoveCardFromDeckAsync(int deckId, int cardId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
@@ -31,6 +33,7 @@ namespace DataAccess.DAOs.Implementations
             return rowsAffected > 0;
         }
 
+        // Retorna la cantidad de cartas que existen en un mazo.
         public async Task<int> GetCardCountInDeckAsync(int deckId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
@@ -38,13 +41,14 @@ namespace DataAccess.DAOs.Implementations
             return await connection.ExecuteScalarAsync<int>(query, new { DeckId = deckId });
         }
 
+        // Obtiene la lista de cartas en un mazo, incluyendo el ID y nombre de cada carta.
         public async Task<IEnumerable<CardDeckResponseDto>> GetCardsInDeckAsync(int deckId)
         {
             using var connection = await _databaseConnection.GetConnectionAsync();
-            var query = @"SELECT c.Id AS CardId, c.Name AS CardName
-                          FROM CardDecks AS cd
-                          INNER JOIN Cards AS c ON cd.CardID = c.Id
-                          WHERE cd.DeckID = @DeckId";
+            var query = @" SELECT c.Id AS CardId, c.Name AS CardName
+                           FROM CardDecks AS cd
+                           INNER JOIN Cards AS c ON cd.CardID = c.Id
+                           WHERE cd.DeckID = @DeckId";
             return await connection.QueryAsync<CardDeckResponseDto>(query, new { DeckId = deckId });
         }
     }
